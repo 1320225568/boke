481f3baf3271764ea4f3539f6295ebbceaca956a
dahui <1320225568@qq.com>
2019-01-01 05:32:51 +0800

    添加logger日志包

---
+++ src/main/java/com/dahui/myboke/common/BaseController.java
@@ -0,0 +1,14 @@
+package com.dahui.myboke.common;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @program: code
+ * @description: 公共的Controller层
+ * @author: Dahui
+ * @create: 2019-01-01 03:16
+ **/
+public class BaseController {
+
+}
--- src/main/java/com/dahui/myboke/controller/IndexController.java
+++ src/main/java/com/dahui/myboke/controller/IndexController.java
@@ -1,6 +1,18 @@
 package com.dahui.myboke.controller;
 
-public class IndexController {
+import com.dahui.myboke.common.BaseController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
+public class IndexController extends BaseController{
+    private Logger logger = LoggerFactory.getLogger(BaseController.class);
+    @RequestMapping(value="/hello" , method = RequestMethod.GET)
+    public String index(){
+        logger.info("进入了index方法！");
+        return "Hello";
+    }
 }
